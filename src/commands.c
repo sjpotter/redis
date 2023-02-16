1192f43b784cf0e6f81806027b86ead6329d8c2d
Hanna Fadida <hanna.fadida@redislabs.com>
2023-02-16 17:37:03 +0200

    fix

--- src/commands.c
+++ src/commands.c
@@ -5999,9 +5999,10 @@ struct jsonObjectElement SRANDMEMBER_ReplySchema_oneOf_2_elements[] = {
 {JSON_TYPE_STRING,"type",.value.string="array"},
 {JSON_TYPE_BOOLEAN,"uniqueItems",.value.boolean=1},
 {JSON_TYPE_OBJECT,"items",.value.object=&SRANDMEMBER_ReplySchema_oneOf_2_items},
+{JSON_TYPE_INTEGER,"minItems",.value.integer=1},
 };
 
-struct jsonObject SRANDMEMBER_ReplySchema_oneOf_2 = {SRANDMEMBER_ReplySchema_oneOf_2_elements,.length=4};
+struct jsonObject SRANDMEMBER_ReplySchema_oneOf_2 = {SRANDMEMBER_ReplySchema_oneOf_2_elements,.length=5};
 
 /* SRANDMEMBER_ReplySchema_oneOf_3 reply schema */
 struct jsonObjectElement SRANDMEMBER_ReplySchema_oneOf_3_elements[] = {
