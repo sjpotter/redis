73164f850df8a8fe4412a9e18075d462a73836c3
Shaya Potter <shaya@redislabs.com>
2023-02-19 11:08:06 +0200

    revert numsub change to map and make schema just be a plain array

--- src/pubsub.c
+++ src/pubsub.c
@@ -625,7 +625,7 @@ NULL
         /* PUBSUB NUMSUB [Channel_1 ... Channel_N] */
         int j;
 
-        addReplyMapLen(c, c->argc-2);
+        addReplyArrayLen(c,(c->argc-2)*2);
         for (j = 2; j < c->argc; j++) {
             list *l = dictFetchValue(server.pubsub_channels,c->argv[j]);
 
@@ -645,7 +645,7 @@ NULL
         /* PUBSUB SHARDNUMSUB [ShardChannel_1 ... ShardChannel_N] */
         int j;
 
-        addReplyMapLen(c, c->argc-2);
+        addReplyArrayLen(c, (c->argc-2)*2);
         for (j = 2; j < c->argc; j++) {
             list *l = dictFetchValue(server.pubsubshard_channels, c->argv[j]);
 
